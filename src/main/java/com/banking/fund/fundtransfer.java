122a405be12a2fad5fe22f89115c568f8d50e314
Admin <Admin@DESKTOP-T0QIQT9>
2023-09-27 17:52:03 +0530

    commit2

---
+++ src/main/java/com/banking/fund/fundtransfer.java
@@ -0,0 +1,11 @@
+package com.banking.fund;
+
+import org.testng.annotations.Test;
+
+public class fundtransfer {
+@Test
+public void test1()
+{
+	System.out.println("---transaction successful---");
+}
+}
